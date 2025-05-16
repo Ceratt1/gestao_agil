d3cc1fc6e6fad66638c7bc4e9b581cd6776d7ca2
ceratti <ceratti@gmail.com>
2025-05-16 11:46:33 -0300

    Add global styles, TypeScript configuration, and new placeholder images
    - Introduced `globals.css` for global styling with light and dark themes.
    - Added `tsconfig.json` for TypeScript configuration with strict settings.
    - Included new placeholder images `placeholder3.jpg` and `placeholder4.jpg`.

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,23 @@
+import Header from "@/components/header/header"
+import Hero from "@/components/hero/hero"
+import FeaturedProducts from "@/components/products/feature-products"
+import AboutUs from "@/components/about-us/about-us"
+import Footer from "@/components/footer/footer"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white">
+      <Header />
+      <Hero />
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center px-4">
+          <div className="w-full border-t border-gray-200"></div>
+        </div>
+
+      </div>
+      <FeaturedProducts />
+      <AboutUs />
+      <Footer />
+    </main>
+  )
+}
