09ca12e95334d8065aef9885e2e3d3cf6f3f80db
Eduardo Paim <zeedu223@gmail.com>
2025-05-17 17:16:09 -0300

    Alteracao no models, paar a ciracao do banco de dados

--- backend/loja/models.py
+++ backend/loja/models.py
@@ -3,16 +3,35 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 import uuid
 class Produto(models.Model):
+    class Categoria(models.TextChoices):
+        BOLSO = 'Bolso', 'Bolso'
+        MESA = 'Mesa', 'Mesa'
+        PAREDE = 'Parede', 'Parede'
+        PULSO = 'Pulso', 'Pulso'
+        TORRE = 'Torre', 'Torre'
+        TODAS = 'Todas', 'Todas'
+        CORDA = 'Corda', 'Corda'
+        MECANICO_AUTOMATICO = 'Automatico', 'Automatico'
+        MECANICO_BATERIA = 'Bateria', 'Bateria'
+        SOLAR = 'Solar', 'Solar'
+        QUARTZO = 'Quartzo', 'Quartzo'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     titulo = models.CharField(max_length=255)
     descricao = models.TextField()
     caminho_imagem = models.CharField(max_length=512)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.CharField(
+        max_length=20,
+        choices=Categoria.choices,
+        default=Categoria.TODAS
+    )
 
     def __str__(self):
         return self.titulo
 
 class Usuario(AbstractUser):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True, primary_key=True)
     class Regra(models.TextChoices):
         ADMIN = 'ADMIN', 'Admin'
         REGULAR = 'REGULAR', 'Regular'
