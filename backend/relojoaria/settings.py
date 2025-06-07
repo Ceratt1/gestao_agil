886d8146efce1a1217ba39a1bfb7e3ba1b706581
Rene Arthur <rene_arthur@outlook.com>
2025-06-07 16:26:38 -0300

    corrigido media_url

--- backend/relojoaria/settings.py
+++ backend/relojoaria/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -134,3 +134,4 @@ AWS_S3_REGION_NAME = os.environ.get("BUCKETEER_AWS_REGION")
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
