dae564a0c2565651d637ea90d5700747316690aa
Rene Arthur <rene_arthur@outlook.com>
2025-06-07 17:19:58 -0300

    testando configuração

--- backend/relojoaria/settings.py
+++ backend/relojoaria/settings.py
@@ -134,6 +134,7 @@ AWS_S3_REGION_NAME = os.environ.get("BUCKETEER_AWS_REGION")
 
 AWS_DEFAULT_ACL = 'public-read'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
