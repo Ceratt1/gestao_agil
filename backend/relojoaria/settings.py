bcc8facbd55d7ed60d85d35eac6831cee2e6250c
Rene Arthur <rene_arthur@outlook.com>
2025-06-07 16:05:58 -0300

    corrigido duplicação

--- backend/relojoaria/settings.py
+++ backend/relojoaria/settings.py
@@ -134,6 +134,3 @@ AWS_S3_REGION_NAME = os.environ.get("BUCKETEER_AWS_REGION")
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
